0b7eda4e6fec0162bf5f86ee32d6acdde33c792f
Jess0420 <53028568+Jess0420@users.noreply.github.com>
2024-10-07 13:28:25 -0400

    Updates

---
+++ precache-manifest.5ced1856999311d61b0a87399421cf95.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22ad8d2500da9723089c51e65960e482",
+    "url": "/jess_medina/index.html"
+  },
+  {
+    "revision": "e6f9fdb9977d0e03db36",
+    "url": "/jess_medina/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "aea54c2cc0e9daa36ce6",
+    "url": "/jess_medina/static/js/2.bbc244fd.chunk.js"
+  },
+  {
+    "revision": "ea935f47a7b92aa14dc7",
+    "url": "/jess_medina/static/js/3.a29efab2.chunk.js"
+  },
+  {
+    "revision": "e6f9fdb9977d0e03db36",
+    "url": "/jess_medina/static/js/main.15f95cfc.chunk.js"
+  },
+  {
+    "revision": "6437b40b56c209e5753d",
+    "url": "/jess_medina/static/js/runtime~main.0f095f56.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/jess_medina/static/media/logo.06e73328.svg"
+  }
+]);
