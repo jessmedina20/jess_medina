a67a1b16220151853ac3bf2092e8c625106501b3
Jess0420 <53028568+Jess0420@users.noreply.github.com>
2024-10-07 13:30:58 -0400

    change to homepage

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. 
+          Here is some text, I hope it works
         </p>
         <a
           className="App-link"
