d1a48c1785313a8a0584b83dbd8b5a79672ecbde
Jess0420 <53028568+Jess0420@users.noreply.github.com>
2024-12-05 19:21:23 -0500

    Updates

---
+++ precache-manifest.a4585f486366774f38a94b15de3eb9da.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f69774dfa23baca9c0cc6c55b1a76f92",
+    "url": "/jess_medina/index.html"
+  },
+  {
+    "revision": "ffd0dff1e9d72e0aa580",
+    "url": "/jess_medina/static/css/main.b38fb31f.chunk.css"
+  },
+  {
+    "revision": "e2a5501e3535b90207f0",
+    "url": "/jess_medina/static/js/2.aefe4ebc.chunk.js"
+  },
+  {
+    "revision": "09dfcff8991931712817539b48cc9b6c",
+    "url": "/jess_medina/static/js/2.aefe4ebc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd35444a22c834aa9443",
+    "url": "/jess_medina/static/js/3.6c84313d.chunk.js"
+  },
+  {
+    "revision": "ffd0dff1e9d72e0aa580",
+    "url": "/jess_medina/static/js/main.55186b0e.chunk.js"
+  },
+  {
+    "revision": "806ed796cc963b7e468b",
+    "url": "/jess_medina/static/js/runtime-main.107b69de.js"
+  },
+  {
+    "revision": "c9fddbd0147e5e89e9e05b4b157468c4",
+    "url": "/jess_medina/static/media/IMG_3626.c9fddbd0.JPG"
+  }
+]);
